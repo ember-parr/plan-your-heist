50740604a59d92213455a8b409b25f50a0f6428f
Ember Parr <ember.devops@gmail.com>
2020-11-20 11:13:17 -0600

    added simple try catch around entire TeamMemberSetup method

--- Program.cs
+++ Program.cs
@@ -14,18 +14,25 @@ namespace PlanYourHeist
 
         static void TeamMemberSetup()
         {
-            Console.Write("Enter Your Name: ");
-            string NameResponse = Console.ReadLine();
-            Console.WriteLine(" ");
-            Console.Write("What Is Your Skill Level? (enter a number 1-100): ");
-            string SkillResponse = Console.ReadLine();
-            Console.WriteLine(" ");
-            Console.Write("How Couragous Are You? (enter a number 0.0-2.0): ");
-            string CourageResponse = Console.ReadLine();
-            Console.WriteLine(" ");
+            try
+            {
+                Console.Write("Enter Your Name: ");
+                string NameResponse = Console.ReadLine();
+                Console.WriteLine(" ");
+                Console.Write("What Is Your Skill Level? (enter a number 1-100): ");
+                string SkillResponse = Console.ReadLine();
+                Console.WriteLine(" ");
+                Console.Write("How Couragous Are You? (enter a number 0.0-2.0): ");
+                string CourageResponse = Console.ReadLine();
+                Console.WriteLine(" ");
 
-            TeamMember Player = new TeamMember(NameResponse, int.Parse(SkillResponse), double.Parse(CourageResponse));
-            Console.WriteLine($"{Player.GetTeamMember()}");
+                TeamMember Player = new TeamMember(NameResponse, int.Parse(SkillResponse), double.Parse(CourageResponse));
+                Console.WriteLine($"{Player.GetTeamMember()}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("ERROR: Be sure to use correct values when entering a team member. Please Try again.");
+            };
         }
     }
 }
